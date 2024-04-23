c782fbd4a3efed9d2f57bfa20b62535bf6cd5199
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-23 00:20:00 -0400

    finalized bert workflow

--- analysis/bundle/clustering.py
+++ analysis/bundle/clustering.py
@@ -5,14 +5,13 @@ from analysis.bundle.models.lda import LDAModel
 from analysis.bundle.models.lsi import LSIModel
 from analysis.bundle.models.bert.bert_topic import BERTModel
 
-# TODO - Use BERTopic for clustering
-# from bertopic import BERTopic
-
 get_class = lambda x: globals()[x]
 
 
 def cluster_by_topic(model_name: str, documents: List[str], num_clusters: int):
-    processed_docs = [preprocess(doc) for doc in documents] if model_name != 'bert' else documents
+    processed_docs = (
+        [preprocess(doc) for doc in documents] if model_name != "bert" else documents
+    )
     model: BaseModel = get_class(f"{model_name.upper()}Model")(
         processed_docs, num_clusters
     )
--- analysis/bundle/models/bert/bert_topic.py
+++ analysis/bundle/models/bert/bert_topic.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 from bertopic import BERTopic
@@ -5,8 +6,9 @@ from analysis.bundle.models.base_model import BaseModel
 from analysis.bundle.models.bert.constants import BERT_MODEL_PATH
 from sentence_transformers import SentenceTransformer
 
+TOPIC_THRESHOLD = 0.5
 
-# TODO - complete
+
 class BERTModel(BaseModel):
     def __init__(self, documents: List[str], num_clusters) -> None:
         super().__init__(documents, num_clusters)
@@ -17,12 +19,21 @@ class BERTModel(BaseModel):
         )
 
     def create_vector(self, document: str) -> List[float]:
-        # Do not need vector for BERTopic
         return document
 
     def cluster(self) -> tuple[dict[int, List[int]], dict[int, List[str]]]:
-        topics, probs = self.bert_model.transform(self.documents)
-        print(f"Topics: {topics}")
-        print(f"Probabilities: {probs}")
-        import pdb;pdb.set_trace()
-        raise NotImplementedError("cluster method is not completely implemented")
+        pred_topics, probs = self.bert_model.transform(self.documents)
+
+        clusters = defaultdict(list)
+        dt_topics = dict()
+
+        for i, (topic, prob) in enumerate(zip(pred_topics, probs)):
+            if prob >= TOPIC_THRESHOLD:
+                clusters[topic].append(i)
+
+                if topic not in dt_topics:
+                    dt_topics[topic] = [
+                        keyword for keyword, _ in self.bert_model.get_topic(topic)
+                    ]
+
+        return clusters, dt_topics
